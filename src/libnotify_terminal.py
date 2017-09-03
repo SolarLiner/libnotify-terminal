98dfb243ff0cbe5b5c67a311b85733e1f5d184ee
Nathan Graule <solarliner@gmail.com>
2017-09-03 15:28:04 +0200

    Extracted argument getter function

--- src/libnotify_terminal.py
+++ src/libnotify_terminal.py
@@ -77,34 +77,14 @@ def main():
     reply_to = "the message"
     message = "Reply to the received message"
 
+
+
     args = os.sys.argv
 
     if len(args) < 3:
         os.sys.exit(-1)
 
-    for i in range(1, len(args)):
-        if args[i] == "--app-title":
-            app_title = args[i+1]
-        elif args[i] == "--title":
-            title = args[i+1]
-        elif args[i] == "--subtitle":
-            subtitle = args[i+1]
-        elif args[i] == "--body":
-            body = args[i+1]
-        elif args[i] == "--action":
-            action = args[i+1].split(',')
-            actions.append({
-                'name': action[0],
-                'label': action[1]
-            })
-        elif args[i] == "--reply":
-            is_reply = True
-        elif args[i] == "--reply-to":
-            reply_to = args[i+1]
-        elif args[i] == "--reply-message":
-            message = args[i+1]
-        else:
-            pass
+    app_title, body, is_reply, message, reply_to, subtitle, title = get_args(args)
 
     if not body:
         os.sys.stderr.write("{0} needs a body.".format(args[0]))
@@ -118,6 +98,33 @@ def main():
         n.add_action(action['name'], action['label'])
 
     n.show()
+
+
+def get_args(args):
+    for i in range(1, len(args)):
+        if args[i] == "--app-title":
+            app_title = args[i + 1]
+        elif args[i] == "--title":
+            title = args[i + 1]
+        elif args[i] == "--subtitle":
+            subtitle = args[i + 1]
+        elif args[i] == "--body":
+            body = args[i + 1]
+        elif args[i] == "--action":
+            action = args[i + 1].split(',')
+            actions.append({
+                'name': action[0],
+                'label': action[1]
+            })
+        elif args[i] == "--reply":
+            is_reply = True
+        elif args[i] == "--reply-to":
+            reply_to = args[i + 1]
+        elif args[i] == "--reply-message":
+            message = args[i + 1]
+        else:
+            pass
+    return app_title, body, is_reply, message, reply_to, subtitle, title
 
 
 if __name__ == '__main__':
