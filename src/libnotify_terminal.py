7d5de985e682f80a7266c9e6a158781829b11255
Nathan Graule <solarliner@gmail.com>
2017-09-03 18:27:51 +0200

    Use module argparser to parse cmd arguments (#16)
    * Imported argparse
    
    * Changed get_args to use argparse
    
    * AutoPEP8
    
    * Fix parser getting script filename as first argument
    
    * Fix overwriting builtin 'id'
    
    * Update Travis config to add python endpoints

--- src/libnotify_terminal.py
+++ src/libnotify_terminal.py
@@ -1,13 +1,14 @@
 import os
 # Gtk imports
 import notify2 as notify
+import argparse
 from gi.repository import GObject as gobject
 
 
 class Notifier(object):
     def __init__(self, application_title, title, subtitle, body):
         if subtitle:
-            body = subtitle+'\n'+body
+            body = subtitle + '\n' + body
 
         notify.init(application_title, mainloop='glib')
 
@@ -68,57 +69,66 @@ class Notifier(object):
 
 
 def main():
-    app_title = "libnotify-terminal"
-    title = "Notification"
-    subtitle = None
-    body = None
-    actions = []
-    is_reply = False
-    reply_to = "the message"
-    message = "Reply to the received message"
+    args = get_args(os.sys.argv)
+    n = Notifier(args.app_title, args.title, args.subtitle, args.body)
 
-    args = os.sys.argv
-
-    if len(args) < 3:
-        os.sys.exit(-1)
-
-    for i in range(1, len(args)):
-        if args[i] == "--app-title":
-            app_title = args[i+1]
-        elif args[i] == "--title":
-            title = args[i+1]
-        elif args[i] == "--subtitle":
-            subtitle = args[i+1]
-        elif args[i] == "--body":
-            body = args[i+1]
-        elif args[i] == "--action":
-            action = args[i+1].split(',')
-            actions.append({
-                'name': action[0],
-                'label': action[1]
-            })
-        elif args[i] == "--reply":
-            is_reply = True
-        elif args[i] == "--reply-to":
-            reply_to = args[i+1]
-        elif args[i] == "--reply-message":
-            message = args[i+1]
-        else:
-            pass
-
-    if not body:
-        os.sys.stderr.write("{0} needs a body.".format(args[0]))
-        os.sys.exit(-1)
-
-    n = Notifier(app_title, title, subtitle, body)
-
-    if is_reply:
-        n.add_reply_action("Reply", reply_to, message)
-    for action in actions:
-        n.add_action(action['name'], action['label'])
+    if args.is_reply:
+        n.add_reply_action("Reply", args.reply_to, args.message)
+    for action in args.actions:
+        assert isinstance(action, str)
+        split_actions = action.split(',')
+        assert len(split_actions) == 2
+        action_id, label = split_actions
+        n.add_action(action_id, label)
 
     n.show()
 
 
+def get_args(args):
+    parser = argparse.ArgumentParser(description='Fires notifications with callbacks from the command-line.')
+    parser.add_argument_group(title='Notification')
+    parser.add_argument('-b', '--body', dest='body', help='Notification body', required=True)
+    parser.add_argument('--app-title', dest='app_title', nargs='?', help='Application title',
+                        default='libnotify-terminal')
+    parser.add_argument('-t', '--title', dest='title', nargs='?', help='Notification title', default='Notification')
+    parser.add_argument('-s', '--subtitle',
+                        dest='subtitle',
+                        nargs='?',
+                        help='Notification subtitle (defined as the first line in the body, in bold)',
+                        default=None
+                        )
+    parser.add_argument('--action',
+                        dest='actions',
+                        nargs='*',
+                        help="Add action in the form {id},{label}, where label is the display name of the button. "
+                             "When no actions are passed, %(prog)s returns directly. If an action has been specified, "
+                             "it will wait for either timeout or callback. This is because on most (if not all) DEs, "
+                             "after the notification has disappeared on-screen, if it can be found on a notification "
+                             "tray, buttons will not be shown.",
+                        default=[]
+                        )
+    parser.add_argument_group(title='Reply')
+    parser.add_argument('-r', '--reply',
+                        dest='is_reply',
+                        help='Flag notification as reply-able. This will internally use zenity to ask the user for a reply.',
+                        action='store_true'
+                        )
+    parser.add_argument('--reply-to',
+                        dest='reply_to',
+                        nargs='?',
+                        help='Recipient of the message to be sent. For display purposes only.',
+                        default=None
+                        )
+    parser.add_argument('--reply-message',
+                        dest='message',
+                        nargs='?',
+                        help='Original message. For display purposes only. Can be different than message body.',
+                        default=None
+                        )
+
+    return parser.parse_args(args[1:])
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    print(get_args(os.sys.argv))
